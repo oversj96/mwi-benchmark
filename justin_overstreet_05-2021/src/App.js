fe6f585b966b9680c86bee837c9db228a5ed7fc9
oversj96 <45669103+oversj96@users.noreply.github.com>
2021-05-30 13:49:11 -0500

    firstname_lastname-MM-YYYY

--- justin_overstreet_05-2021/src/App.js
+++ justin_overstreet_05-2021/src/App.js
@@ -1,7 +1,9 @@
 import MwiBenchmarkView from "../src/components/MwiBenchmarkView";
 
+// Just to be extra technical, this comment is so I can make a
+// commit with the generic "firstname_lastname-MM-YYYY" as well.
 const App = () => {
-  return <MwiBenchmarkView />;
+  return <MwiBenchmarkView />; 
 }; 
 
 export default App;
